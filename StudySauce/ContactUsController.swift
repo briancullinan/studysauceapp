7041b162bfa68e402e7fbe64b3b16a0e782b205b
Stephen Houghton <stephen@studysauce.com>
2015-10-07 17:53:05 -0700

    Contact us form (still need to wire functionality)

--- StudySauce/ContactUsController.swift
+++ StudySauce/ContactUsController.swift
@@ -12,14 +12,20 @@ import CoreData
 import UIKit
 
 class ContactUsController: UIViewController {
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.message.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).CGColor
+        self.message.layer.borderWidth = 1.0
+        self.message.layer.cornerRadius = 5
+    }
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var email: UITextField!
-    @IBOutlet weak var message: UITextField!
+    @IBOutlet weak var message: UITextView!
     
     @IBAction func sendEmail(sender: AnyObject) {
     }
     @IBAction func DismissKeyboard(sender: AnyObject) {
     }
     
+   
 }
