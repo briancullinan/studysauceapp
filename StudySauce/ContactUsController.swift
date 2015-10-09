f6516a55c149111ac7b1a85bc15d29f6320e04eb
Stephen Houghton <stephen@studysauce.com>
2015-10-08 20:20:28 -0700

    Contact us wiring

--- StudySauce/ContactUsController.swift
+++ StudySauce/ContactUsController.swift
@@ -10,20 +10,55 @@
 import Foundation
 import CoreData
 import UIKit
+import MessageUI
 
-class ContactUsController: UIViewController {
+class ContactUsController: UIViewController, MFMailComposeViewControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.message.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).CGColor
         self.message.layer.borderWidth = 1.0
         self.message.layer.cornerRadius = 5
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var message: UITextView!
     
     @IBAction func sendEmail(sender: AnyObject) {
+        
+        let toRecipients = ["admin@studysauce.com"]
+        
+        let mc: MFMailComposeViewController = MFMailComposeViewController()
+        mc.mailComposeDelegate = self
+        mc.setSubject("Contact Us: " + self.name.text!)
+        mc.setMessageBody(self.message.text, isHTML: false)
+        mc.setToRecipients(toRecipients)
+
+        self.presentViewController(mc, animated: true, completion: nil)
+        
     }
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        switch result.rawValue {
+        case MFMailComposeResultCancelled.rawValue:
+            print("Mail Cancelled")
+        case MFMailComposeResultSaved.rawValue:
+            print("Mail Saved")
+        case MFMailComposeResultSent.rawValue:
+            print("Mail Sent")
+        case MFMailComposeResultFailed.rawValue:
+            print("Mail Sent Failure: \(error!.localizedDescription)")
+        default:
+            break
+        }
+        controller.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    
     @IBAction func DismissKeyboard(sender: AnyObject) {
     }
     
